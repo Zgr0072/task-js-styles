5d524c111e9ab849eea90e111e8ab77f2f29273f
Zgr0072 <grigoryzim@yandex.ru>
2022-04-19 19:58:16 +0300

    Задание по стилям

--- src/Styles.js
+++ src/Styles.js
@@ -11,10 +11,22 @@
    elemСlass - название класса, которое проверяется.
 */
 
-export function hasClass(elem, elemClass) {}
+export function hasClass(elem, elemClass) {
+    return elem.classList.contains(elemClass);
+}
 
-export function addClass(elem, elemClass) {}
+export function addClass(elem, elemClass) {
+    if (!hasClass(elem, elemClass)) {
+        elem.classList.add(elemClass);
+    }
+}
 
-export function removeClass(elem, elemClass) {}
+export function removeClass(elem, elemClass) {
+    if (hasClass(elem, elemClass)) {
+        elem.classList.remove(elemClass);
+    }
+}
 
-export function toggleClass(elem, elemClass) {}
+export function toggleClass(elem, elemClass) {
+    elem.classList.toggle(elemClass);
+}
